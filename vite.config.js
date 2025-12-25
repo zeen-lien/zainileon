be04838cb014b2028cb8bb4bbf0401b0a37593ec
zeen-lien <your-email@example.com>
2025-12-25 22:25:16 +0700

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landing-page/', // Sesuaikan dengan nama repository GitHub
+  base: '/zainileon/', // Sesuaikan dengan nama repository GitHub
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
